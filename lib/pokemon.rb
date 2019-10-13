b659df979d8c1627a0b79becb9f8c971146338a1
Mike McTighe <m.mctighe10@gmail.com>
2019-10-13 18:51:25 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -11,9 +11,9 @@ attr_accessor :id, :name, :type, :db
 
   def self.save(name, type, db)
     sql = <<-SQL
-    INNSERT INTO pokemon (name, type) VALUES (?, ?)
+    INSERT INTO pokemon (name, type) VALUES (?, ?)
     SQL
 db.execute(sql, name, type)
 
-end 
+end
 end
