f8d2f9340a31f2c95c9abeea7c5d633c4b93c89e
Mike McTighe <m.mctighe10@gmail.com>
2019-10-13 18:36:31 +0000

    Automatically backed up by Learn

--- spec/db_spec.rb
+++ spec/db_spec.rb
@@ -1,6 +1,6 @@
 describe "schema_migration.sql" do
   before do
-    @db = SQLite3::Database.new('./p')
+    @db = SQLite3::Database.new('./poke')
     @sql_runner = SQLRunner.new(@db)
     @sql_runner.execute_schema_migration_sql
   end
